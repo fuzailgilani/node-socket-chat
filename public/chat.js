0399766724c325ee832b31d1ad90b843becd8b79
Fuzail <fuzailgilani@gmail.com>
2018-08-20 15:09:12 -0500

    added functionality to see if user is typing

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 // Emit events
 
@@ -16,7 +17,16 @@ btn.addEventListener('click', () => {
   });
 });
 
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+});
+
 // Listen for events
 socket.on('chat', (data) => {
   output.innerHTML += '<p><strong>' + data.handle + ': </strong> ' + data.message + '</p>';
+  feedback.innerHTML = '';
+});
+
+socket.on('typing', (data) => {
+  feedback.innerHTML = '<p><em>' + data + 'is typing a message...</em></p>';
 });
